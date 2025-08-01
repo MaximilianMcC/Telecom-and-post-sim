e8b273aac2656412d61cd0dcc1027512ec812204
MaximilianMcC <max@maximilian.co.nz>
2025-08-01 23:53:08 +1200

    cheap as morse code test because i really wanna start writing ts but im not bothered to make smoke work rn

---
+++ src/Program.cs
@@ -0,0 +1,16 @@
+﻿using Raylib_cs;
+
+public class Program
+{
+	public static void Main(string[] args)
+	{
+		Raylib.InitWindow(512, 512, "jonh spark, inventor of telegraphs");
+
+		while (Raylib.WindowShouldClose() == false)
+		{
+			
+		}
+
+		Raylib.CloseWindow();
+	}
+}
